a8f9d64aaac3650c3c89328c7f3ee43b5bff93b4
Jay Beavers <jay@hikinghomeschoolers.org>
2018-11-08 11:26:57 -0800

    Add support for creating hard and symbolic links

--- lib/Microsoft.HandsFree.Win32/Kernel32.cs
+++ lib/Microsoft.HandsFree.Win32/Kernel32.cs
@@ -16,5 +16,20 @@ namespace Microsoft.HandsFree.Win32
         [DllImport(DllName, SetLastError = true)]
         public static extern IntPtr LocalFree(IntPtr hMem);
 
+        // CreateHardLink(@"c:\temp\New Link", @"c:\temp\Original File",IntPtr.Zero);
+        [DllImport("Kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool CreateHardLink(string lpFileName, string lpExistingFileName, IntPtr lpSecurityAttributes);
+
+        // CreateSymbolicLink(symbolicLink, directoryName, SymbolicLink.Directory);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, SymbolicLink dwFlags);
+
+        public enum SymbolicLink
+        {
+            File = 0,
+            Directory = 1,
+            FileUnprivileged = 2,
+            DirectoryUnprivileged = 3
+        }
     }
 }
